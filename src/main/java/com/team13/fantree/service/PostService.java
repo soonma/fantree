9a1fa03f4a11b3dc30ee366583b1ad3345dc990d
silicao3o <zergskybmw@gmail.com>
2024-06-04 17:08:43 +0900

    Feat SignUp & WithDraw

--- src/main/java/com/team13/fantree/service/PostService.java
+++ src/main/java/com/team13/fantree/service/PostService.java
@@ -1,9 +1,12 @@
 package com.team13.fantree.service;
 
+import com.team13.fantree.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
+
+    PostRepository postRepository;
 }
