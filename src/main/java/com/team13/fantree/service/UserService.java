3fe07fc2f72b83b80f728d5a2c947f5c761e348e
dobam <ckehqja@gmail.com>
2024-06-04 17:58:01 +0900

    update : repository

--- src/main/java/com/team13/fantree/service/UserService.java
+++ src/main/java/com/team13/fantree/service/UserService.java
@@ -3,8 +3,10 @@ package com.team13.fantree.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import com.team13.fantree.repository.UserRepository;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
+	private final UserRepository userRepository;
 }
