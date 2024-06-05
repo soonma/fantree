c20fe4b5862714a056a9c7824134181569fdd1f9
silicao3o <zergskybmw@gmail.com>
2024-06-05 15:32:15 +0900

    Fix : Application.properties Add

--- src/main/java/com/team13/fantree/exception/UserErrorCode.java
+++ src/main/java/com/team13/fantree/exception/UserErrorCode.java
@@ -13,6 +13,7 @@ public enum UserErrorCode implements ErrorCode {
 	PW_MISMATCH(HttpStatus.NOT_FOUND,
 		"유저이름과 비밀번호 불일치");
 
+
 	private final HttpStatus httpStatus;
 	private final String message;
 }
