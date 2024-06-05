e4b53a60d1636adb46ae6992dce28cd6bb24b329
silicao3o <zergskybmw@gmail.com>
2024-06-05 10:44:32 +0900

    Fix UserService Pattern -> SingUpReaquestDto

--- src/main/java/com/team13/fantree/dto/SignUpRequestDto.java
+++ src/main/java/com/team13/fantree/dto/SignUpRequestDto.java
@@ -3,14 +3,21 @@ package com.team13.fantree.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class SignUpRequestDto {
     @NotBlank
+    @Pattern(regexp = "\"^[a-zA-Z0-9_-]{10,20}$\"",
+            message = "ID 형식이 올바르지 않습니다.")
     private String username;
+
     @NotBlank
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{10,}$",
+            message = "대소문자 포함 영문 + 숫자 + 특수문자를 최소 1글자씩 포함, 최소 10글자 이상")
     private String password;
+
     private String name;
     @Email
     private String email;
--- src/main/java/com/team13/fantree/service/UserService.java
+++ src/main/java/com/team13/fantree/service/UserService.java
@@ -1,6 +1,5 @@
 package com.team13.fantree.service;
 
-
 import com.team13.fantree.dto.SignUpRequestDto;
 import com.team13.fantree.entity.User;
 import com.team13.fantree.entity.UserStatusEnum;
@@ -10,36 +9,19 @@ import lombok.RequiredArgsConstructor;
 
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.team13.fantree.dto.LoginRequestDto;
-
-
-
-
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
 
     private final UserRepository userRepository;
 
     public void signup(SignUpRequestDto requestDto){
         String username = requestDto.getUsername();
 
-        if(!isValidUsername(requestDto.getUsername())){
-            throw new IllegalArgumentException("ID 형식이 올바르지 않습니다.");
-        }
-
         if (userRepository.findByUsername(username).isPresent()){
             throw new IllegalArgumentException("중복된 회원입니다.");
         }
-
-//        if(!isValidPasswordFormat(requestDto.getPassword())){
-//            throw new IllegalArgumentException("비밀번호 형식이 올바르지 않습니다.");
-//        }
 
         User user = new User(requestDto);
         userRepository.save(user);
@@ -57,16 +39,6 @@ public class UserService {
         user.withDraw();
     }
 
-    private boolean isValidPasswordFormat(String password){
-        String passwordPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=[\\]{};':\"\\\\|,.<>/?]).{10,}$";
-        return password.matches(passwordPattern);
-    }
-
-    private boolean isValidUsername(String username){
-        String usernamePattern = "^[a-zA-Z0-9_-]{10,20}$";
-        return username.matches(usernamePattern);
-    }
-
 	public boolean login(LoginRequestDto requestDto) {
 		User findUser = userRepository.findByUsername(requestDto.getUsername()).get();
 		if (findUser == null || !findUser.getPassword().equals(requestDto.getPassword()))
@@ -76,7 +48,6 @@ public class UserService {
 
 	@Transactional
 	public boolean logout(Long id) {
-		this.id = id;
 		User user = userRepository.findById(id).get();
 		return user.logout();
 	}
