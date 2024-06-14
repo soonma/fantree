d4f285a338ed4d6fd844303742df4eefa4ba8835
soonma <clsrn77990@naver.com>
2024-06-14 12:13:38 +0900

    test : Like Test

--- src/test/java/com/team13/fantree/entity/CommentTest.java
+++ src/test/java/com/team13/fantree/entity/CommentTest.java
@@ -1,13 +1,11 @@
 package com.team13.fantree.entity;
 
 import com.team13.fantree.repository.CommentRepository;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Configuration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,12 +27,13 @@ class CommentTest {
         User user = new User("fltnsah","1q2w3e4r","이순모","clsrn77990@naver.com","안녕하세요");
         Post post = new Post("게시글 내용",user);
         Comment comment = new Comment(post,user,"댓글내용");
+
         //when
         Comment saveComment = commentRepository.save(comment);
+
         //then
         assertEquals(saveComment.getUser().getName(),user.getName());
         assertNotNull(saveComment.getId());
     }
-
 
 }
--- src/test/java/com/team13/fantree/entity/LikeTest.java
+++ src/test/java/com/team13/fantree/entity/LikeTest.java
@@ -1,7 +1,33 @@
 package com.team13.fantree.entity;
 
+import com.team13.fantree.repository.LikeRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
 class LikeTest {
+    private final LikeRepository likeRepository;
 
+    @Autowired
+    LikeTest(LikeRepository likeRepository) {
+        this.likeRepository = likeRepository;
+
+    }
+
+    @Test
+    @DisplayName("좋아요 테스트")
+    void likeTest() {
+        //given
+        Like like = new Like(1,1,"comment");
+        //when
+        Like likeSave = likeRepository.save(like);
+        //then
+        assertEquals(likeSave.getContentType(),ContentEnumType.COMMENT);
+    }
 }
