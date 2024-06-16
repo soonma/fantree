05bf4253dda1da5bf0d20a00fbc717698f474b59
soonma <clsrn77990@naver.com>
2024-06-17 00:36:06 +0900

    test : EntityBasicTest

--- src/test/java/com/team13/fantree/entity/PostTest.java
+++ src/test/java/com/team13/fantree/entity/PostTest.java
@@ -1,7 +1,37 @@
 package com.team13.fantree.entity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.team13.fantree.repository.PostRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
 class PostTest {
+    private final PostRepository postRepository;
+
+    @Autowired
+    PostTest(PostRepository postRepository){
+        this.postRepository = postRepository;
+    }
+
+    @Test
+    @DisplayName("Post 관련 테스트")
+    void PostCreateTest(){
+        //given
+        User user = new User("fltnsah","1q2w3e4r","이순모","clsrn77990@naver.com","안녕하세요");
+        Post post = new Post("게시글 내용",user);
+
+        //when
+        Post postSave = postRepository.save(post);
+        //then
+        assertEquals(postSave.getUser().getId(),user.getId());
+    }
+
 
 }
--- src/test/java/com/team13/fantree/entity/UserTest.java
+++ src/test/java/com/team13/fantree/entity/UserTest.java
@@ -1,7 +1,34 @@
 package com.team13.fantree.entity;
 
+import com.team13.fantree.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserTest {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    UserTest(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @DisplayName("User 정보 테스트")
+    @Test
+    void Test(){
+        //given
+        User user =new User("fltnsah","1q2w3e4r","이순모","clsrn77990@naver.com","안녕하세요");
+        //when
+        User userSave = userRepository.save(user);
+        //then
+        assertNotNull(userSave.getId());
+    }
 
 }
