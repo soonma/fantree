7b5b183f5048a70b99b6f835dcad74d20fe7790f
soonma <clsrn77990@naver.com>
2024-06-17 00:45:00 +0900

    test : EmailVaildTest

--- src/test/java/com/team13/fantree/dto/CommentRequestDtoTest.java
+++ src/test/java/com/team13/fantree/dto/CommentRequestDtoTest.java
@@ -1,7 +1,40 @@
 package com.team13.fantree.dto;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommentRequestDtoTest {
+    private static ValidatorFactory factory;
+    private static Validator validator;
+
+    @BeforeAll
+    public static void init() {
+        factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    @DisplayName("테스트")
+    void vaildTest() {
+        //given
+        CommentRequestDto requestDto = new CommentRequestDto();
+        //when
+        Set<ConstraintViolation<CommentRequestDto>> result = validator.validate(requestDto);
+        //then
+        for (ConstraintViolation<CommentRequestDto> violation : result) {
+            assertNotNull(violation.getMessage());
+            System.out.println(violation.getMessage());
+        }
+    }
+
 
 }
--- src/test/java/com/team13/fantree/dto/EmailRequestDtoTest.java
+++ src/test/java/com/team13/fantree/dto/EmailRequestDtoTest.java
@@ -1,7 +1,39 @@
 package com.team13.fantree.dto;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmailRequestDtoTest {
+    private static ValidatorFactory factory;
+    private static Validator validator;
 
+    @BeforeAll
+    public static void init() {
+        factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    @DisplayName("테스트")
+    void vaildTest() {
+        //given
+        EmailRequestDto requestDto = new EmailRequestDto();
+        requestDto.setEmail("111");
+        //when
+        Set<ConstraintViolation<EmailRequestDto>> result = validator.validate(requestDto);
+        //then
+        for (ConstraintViolation<EmailRequestDto> violation : result) {
+            assertNotNull(violation.getMessage());
+            System.out.println("결과: " +violation.getMessage());
+        }
+    }
 }
